8084f23df5c24ab215e774c82d367ee6fdc12858
JIV <JuanVerni@JIV-TB.local>
2017-03-13 14:10:37 -0600

    Explicit db

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 import {MONGODB_URI} from 'react-native-dotenv';
+
 export default () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(MONGODB_URI + '/chats');
+  mongoose.connect('mongodb://heroku_x9fb7pmz:fqv13bdq5o6qk4pstdncnpdv31@ds129600.mlab.com:29600/heroku_x9fb7pmz/chats');
   mongoose.connection
     .once('open', () => console.log('Mongodb running'))
     .on('error', err => console.error(err))
